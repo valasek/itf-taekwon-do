1ce4b458fe1b74d2e2fe744acd43ae8d9ebf639f
Stanislav Valasek <valasek@gmail.com>
2016-07-23 00:23:57 +0200

    Registration page test

--- tests.py
+++ tests.py
@@ -26,14 +26,28 @@ class ItfTestCase(unittest.TestCase):
         result = self.app.get('/')
         self.assertEqual(result.status_code, 200)
 
-    def login(self, username, password):
+    def login(self, email, password):
         return self.app.post('/login', data=dict(
-            email=username,
+            email=email,
             password=password
         ), follow_redirects=True)
 
     def logout(self):
         return self.app.get('/logout', follow_redirects=True)
+
+    def register(self, team, first_name, last_name, email, password, confirm):
+        return self.app.post('/register', data=dict(
+            team=team,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password,
+            confirm=confirm
+        ), follow_redirects=True)
+
+    def test_register(self):
+        rv = self.register('Test tým', 'Jméno', 'Příjmení', 'email@email.com', 'password', 'password')
+        assert b'Registrace proběhla úspěšně.' in rv.data
 
     def test_login_logout(self):
         rv = self.login('admin', 'admin')
